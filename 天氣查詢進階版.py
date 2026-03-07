e4eb27f07aa4654713e793c8a67d74c9330100d5
李名宥 <lmy970112@gmail.com>
2026-03-07 12:16:30 +0800

    Update 天氣查詢進階版.py

--- 天氣查詢進階版.py
+++ 天氣查詢進階版.py
@@ -1,9 +1,10 @@
 import requests
 city=input('Please enter the city you wish to search for:')
-API='30b9dc42d0e560ac0ab5f03e4b1b8430'
+API=input('enter your api key')
 網址=f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}'
 data=(requests.get(網址))
 氣溫=int((data.json()['main']['temp'])-273.15)
 狀況=str(data.json()['weather'][0]['description'])
 國家=str(data.json()['sys']['country'])
 print(f'{國家}的{city}現在的溫度是{氣溫}目前的天氣狀況是{狀況}')
+
